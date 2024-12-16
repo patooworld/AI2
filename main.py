8abfa28a17e8bb206124179ecda6b060f58d01df
SAMUEL CHIBUNNA <info@patooworld.com>
2024-12-16 13:42:57 -0800

    Update main.py

--- main.py
+++ main.py
@@ -26,3 +26,24 @@ print(f"Sentiment: {sentiment}")
 print(f"Summary: {summary}")
 print(f"Detected Objects: {objects}")
 print(f"Prediction: {prediction}")
+
+import os
+
+import google.generativeai as genai
+
+# Using `GOOGLE_API_KEY` environment variable.
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=GOOGLE_API_KEY)
+
+# Checking for all available models with generateContent feature
+for model in genai.list_models():
+    if "generateContent" in model.supported_generation_methods:
+        print(model.name)
+
+# Initiate the Model
+model = genai.GenerativeModel(model_name="gemini")
+
+# Query the Model
+response = model.generate_content("Gemini is a large language model, created by Google")
+
+print(response.text)
