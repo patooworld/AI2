512be6ba9562423330d5b1fe086156272d3b7f96
SAMUEL CHIBUNNA <info@patooworld.com>
2025-02-06 03:34:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+const generationConfig = {
+  temperature: 0.7,
+  candidateCount: 1,
+  topK: 40,
+  topP: 0.95,
+  maxOutputTokens: 1024,
+};
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+const safetySettings = [
+  {
+    category: 'HARM_CATEGORY_DANGEROUS_CONTENT',
+    threshold: 'BLOCK_NONE'
+  },
+];
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-pro",
+});
+
+model.generateContent({
+  generationConfig,
+  safetySettings,
+  contents: [
+    {
+      role: "user",
+      parts: [
+        { text: 'On what planet do humans live? ' }
+    },
+  ],
+}).then(result => {
+  console.log(JSON.stringify(result, null, 2));
+});
